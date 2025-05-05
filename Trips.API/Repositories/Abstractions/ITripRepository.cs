bac0b4d86bf31773722940a2144f6cc73f149b5f
newki <newkirilal@gmail.com>
2025-05-05 22:46:25 +0200

    APBD_07

---
+++ Trips.API/Repositories/Abstractions/ITripRepository.cs
@@ -0,0 +1,13 @@
+﻿using Trips.API.Entities;
+
+namespace Trips.API.Repositories.Abstractions;
+
+public interface ITripRepository
+{
+    public Task<List<Trip>> GetAllTripsAsync(CancellationToken token = default);
+
+    public Task<Trip?> GetTripByIdAsync(int tripId, CancellationToken token = default);
+    public Task<bool> TripExistsAsync(int tripId, CancellationToken token = default);
+    public Task<bool> ClientTripExistsAsync(int clientId, int tripId, CancellationToken token = default);
+    public Task<bool> DeleteClientTripAsync(int clientId, int tripId, CancellationToken token);
+}
---
+++ Trips.API/Services/ClientService.cs
@@ -0,0 +1,47 @@
+﻿using Trips.API.Contracts.Requests;
+using Trips.API.Entities;
+using Trips.API.Exceptions;
+using Trips.API.Repositories.Abstractions;
+using Trips.API.Services.Abstractions;
+
+namespace Trips.API.Services;
+
+public class ClientService : IClientService
+{
+    private readonly IClientRepository _clientRepository;
+    private readonly ITripRepository _tripRepository;
+
+    public ClientService(IClientRepository clientRepository, ITripRepository tripRepository)
+    {
+        _clientRepository = clientRepository;
+        _tripRepository = tripRepository;
+    }
+
+    public async Task<ICollection<ClientTrip>?> GetAllClientTripsAsync(int clientId, CancellationToken token = default)
+        => await _clientRepository.GetClientTripsAsync(clientId, token);
+
+    public async Task<int> CreateClientAsync(CreateClientRequest client, CancellationToken token = default)
+    {
+        var clientExists = await _clientRepository.ClientExistsByPeselAsync(client.Pesel, token);
+        if (clientExists)
+            throw new ClientWithPeselNumberExistsException(client.Pesel);
+
+        var newClient = new Client
+        {
+            Telephone = client.Telephone,
+            FirstName = client.FirstName,
+            LastName = client.LastName,
+            Email = client.Email,
+            Pesel = client.Pesel
+        };
+        
+        var result = await _clientRepository.CreateClientAsync(newClient, token);
+        return result.Id;
+    }
+
+    public async Task<bool> DeleteTourByIdAsync(int userId, int tripId, CancellationToken token = default)
+    {
+       return await _tripRepository.DeleteClientTripAsync(userId, tripId, token);
+    }
+    
+}
